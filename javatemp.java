33f96a7b64d01c2f22ceab313923717e8a876790
Rajan24-N <73275908+Rajan24-N@users.noreply.github.com>
2022-10-13 00:46:21 +0530

    StringTokenizer
    Java for competitive coding have faced a “TLE” even though their logic and complexity are well within the bounds. therefore compress your entire input section to this and then just used FastScanner instead of Scanner:

---
+++ javatemp.java
@@ -0,0 +1,18 @@
+static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try { 
+                                        st=new StringTokenizer(br.readLine());				               
+                                } catch (IOException e) {}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
